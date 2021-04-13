5626137d7e0736167daa61df6832590c211d2870
majimmy88 <majimmy88@gmail.com>
2021-04-12 19:19:39 -0700

    Basic Vue app Running

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <h1>NavBar</h1>
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: 'NavBar',
+  };
+</script>
+
+<style scoped>
+  footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+</style>
