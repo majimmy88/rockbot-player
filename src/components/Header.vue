50a5fd4c6ef2d2e23f4efaf3c060771da9c10e7d
majimmy88 <majimmy88@gmail.com>
2021-04-13 18:39:32 -0700

    Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header-container">
+    <h1>Mini Rockbot</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+  };
+</script>
+
+<style scoped>
+  .header-container {
+    display: flex;
+    justify-content: center;
+    background-color: #33639e;
+    color: #fff;
+    width: 100%;
+  }
+</style>
