324fb41e0a98816a01c7b2b963be8a2087005fbc
majimmy88 <majimmy88@gmail.com>
2021-04-13 18:39:00 -0700

    Now playing component

---
+++ src/components/NowPlaying.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="current">Current Song</div>
+    <h4><u>Coming Up</u></h4>
+    <ul>
+      <li>Song 1</li>
+      <li>Song 2</li>
+      <li>Song 3</li>
+      <li>Song 4</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NowPlaying',
+    props: {
+      songs: {},
+    },
+  };
+</script>
+
+<style scoped>
+  .container {
+    padding: 10px;
+  }
+  .current {
+    border: 1px solid black;
+    border-radius: 5px;
+    height: 50px;
+    margin-bottom: 10px;
+  }
+  ul {
+    list-style-type: none;
+    margin-left: 10px;
+  }
+</style>
