5a6f9eb57da722dab92e7767bf6e015417243927
majimmy88 <majimmy88@gmail.com>
2021-04-13 18:39:11 -0700

    Search component

---
+++ src/components/Search.vue
@@ -0,0 +1,11 @@
+<template>
+  <h4><u>Top Artists</u></h4>
+  <div class="artist-photo-container"></div>
+  <div class="search"></div>
+</template>
+
+<script>
+  export default {
+    name: 'Search',
+  };
+</script>
