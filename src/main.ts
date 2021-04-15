7477083b014665d290350ac64bfd74e05c189e48
majimmy88 <majimmy88@gmail.com>
2021-04-15 00:45:33 -0700

    Added font awesome

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import App from './App.vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-createApp(App).mount("#app");
+library.add(faThumbsUp, faThumbsDown);
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
