d3d41904602abec35fbc6f24f267defdba990cff
majimmy88 <majimmy88@gmail.com>
2021-04-15 00:46:52 -0700

    Song service hooked up to make request from server

--- src/services/SongService.js
+++ src/services/SongService.js
@@ -1,16 +1,13 @@
 import axios from 'axios';
 
-const url = 'https://api.rockbot.com/v3/engage/now_playing';
+const url = 'http://localhost:5000/api/songs';
 
 class SongService {
   //Get Songs
   static getSongs() {
     return new Promise((resolve, reject) => {
       axios
-        .get(url, {
-          headers: { Authorization: '2ab742c917f872aa88644bc8f995e03159b2' },
-          params: { queue: 1 },
-        })
+        .get(url)
         .then((res) => {
           const data = res.data.response;
           resolve(data);
