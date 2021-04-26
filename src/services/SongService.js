3c7c5547f37a147231197d6677248a54953f0f83
majimmy88 <majimmy88@gmail.com>
2021-04-26 11:55:24 -0700

    Fixed song service

--- src/services/SongService.js
+++ src/services/SongService.js
@@ -5,34 +5,30 @@ const url = 'http://localhost:8000/api/songs/';
 //Get Songs from server
 class SongService {
   static getSongs() {
-    return new Promise((resolve, reject) => {
-      axios
-        .get(url)
-        .then((res) => {
-          const data = res.data.response;
-          console.log(data);
-          resolve(data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    });
+    return axios
+      .get(url)
+      .then((res) => {
+        const data = res.data.response;
+        console.log(data);
+        return data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   //Posts either up or down vote for song based on ID and then updates song queue
   static vote(id, data) {
-    return new Promise((resolve, reject) => {
-      axios
-        .post(url + id, data)
-        .then((res) => {
-          const data = res.data.response;
-          console.log(data);
-          resolve(data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    });
+    return axios
+      .post(url + id, data)
+      .then((res) => {
+        const data = res.data.response;
+        console.log(data);
+        return data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
 
