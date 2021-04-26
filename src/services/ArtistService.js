3b5549fbc1365b72d3b4aee1df87b77ebef8889c
majimmy88 <majimmy88@gmail.com>
2021-04-26 14:11:01 -0700

    Fixed artist service

--- src/services/ArtistService.js
+++ src/services/ArtistService.js
@@ -5,63 +5,55 @@ const url = 'http://localhost:8000/api/artists/';
 //Gets top artists from server
 class ArtistService {
   static getArtists() {
-    return new Promise((resolve, reject) => {
-      axios
-        .get(url)
-        .then((res) => {
-          const data = res.data;
-          resolve(data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    });
+    return axios
+      .get(url)
+      .then((res) => {
+        const data = res.data;
+        return data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   //Searches for artists based on search query and returns first 9 results
   static searchArtists(query) {
     // console.log(query);
-    return new Promise((resolve, reject) => {
-      axios
-        .get(url + 'search', { params: query })
-        .then((res) => {
-          const data = res.data;
-          resolve(data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    });
+    return axios
+      .get(url + 'search', { params: query })
+      .then((res) => {
+        const data = res.data;
+        return data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   //Adds song based on artist ID to queue
   static addToQueue(id) {
     console.log(id);
-    return new Promise((resolve, reject) => {
-      axios
-        .post(url + id)
-        .then(() => {
-          console.log(`Data is sent to express`);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    });
+    return axios
+      .post(url + id)
+      .then(() => {
+        console.log(`Data is sent to express`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   static browseArtists(data) {
     // console.log(data);
-    return new Promise((resolve, reject) => {
-      axios
-        .get(url + 'browse', { params: data })
-        .then((res) => {
-          const data = res.data;
-          resolve(data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    });
+    return axios
+      .get(url + 'browse', { params: data })
+      .then((res) => {
+        const data = res.data;
+        return data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
 
